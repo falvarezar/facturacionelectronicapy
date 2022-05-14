6f13a6999d5185b9cad5a2d0bdbd6aa28b4ccc80
Fabian Alvarez <falvarez_arnaldo@hotmail.com>
2022-05-14 19:34:55 -0400

    comentario

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ import cors from 'cors';
 import {CommonRoutesConfig} from './common/common.routes.config';
 import {UsersRoutes} from './users/users.routes.config';
 import debug from 'debug';
-
+// declara las constantes
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
